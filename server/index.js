dcfda720aee713566281561d1561a218357b6ff5
Emmaline <emmalinerose19@gmail.com>
2022-04-28 15:12:05 -0500

    rollbar installed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('html file served successfully.')
 })
 
 const port = process.env.PORT || 4545
@@ -12,3 +13,14 @@ const port = process.env.PORT || 4545
 app.listen(port, () => {
     console.log(`Listening to you on port ${port}`)
 })
+
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '922a1357ef4144d2bc07edfb6abe0a72',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
