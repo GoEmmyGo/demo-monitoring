3f361d28a752f61ea5595b3bcaed946af4455216
Emmaline <emmalinerose19@gmail.com>
2022-04-28 15:27:00 -0500

    rollbar linked

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+const Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '922a1357ef4144d2bc07edfb6abe0a72',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
     rollbar.info('html file served successfully.')
@@ -14,13 +25,3 @@ app.listen(port, () => {
     console.log(`Listening to you on port ${port}`)
 })
 
-// include and initialize the rollbar library with your access token
-const Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: '922a1357ef4144d2bc07edfb6abe0a72',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
-
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
